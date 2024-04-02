3d991f95437c6893e16cd78088202005030b8c49
Daryl Dewilde <daryldewilde@gmail.com>
2024-04-02 09:15:03 +0100

    added further features

--- stocks/App/views.py
+++ stocks/App/views.py
@@ -4,8 +4,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
+from django.http import JsonResponse
 from rest_framework import serializers, status
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login , logout, authenticate
 from datetime import datetime
 from .serializers import *
 
@@ -49,11 +51,18 @@ def createLigneFacture(request):
     quantite = request.GET.get('quantite', 1)
     prix_unitaire = request.GET.get('prix_unitaire', 0)
     total = int(quantite) * float(prix_unitaire)
+    produit = Produit.objects.get(id=produit_id)
+    
 
     data = {'produit': produit_id, 'facture': facture_id, 'quantite': quantite, 'prix_unitaire': prix_unitaire, 'total': total}
     serializer = LigneFactureSerializer(data=data)
 
     if serializer.is_valid():
+        if produit.stock >= int(quantite):
+            produit.stock -= int(quantite)  
+            produit.save()
+        else:
+           return  JsonResponse({'error': 'Stock insufisant', 'restant':produit.stock})
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -61,26 +70,65 @@ def createLigneFacture(request):
 # Your existing views
 
 def produits(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     produits = Produit.objects.all()
     return render(request, 'home/produits.html', {'produits':produits, 'segment':'liste2'})
 
 def qr_code(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     produits = Produit.objects.all()
     return render(request, 'home/qr_code.html', {'produits':produits, 'segment':'code'})
 
 def details_produit(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+
     id  = request.GET.get('id', None)
     produit = Produit.objects.get(id=id)
     return render(request, 'home/details_produit.html', {'produit':produit})
 
 def modif_produit(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    categories = Categorie.objects.all()
+
     if request.method == 'POST':
-        pass
+    # Extract data from the form
+        categorie_nom = request.POST.get('cat')  # Assuming the value of 'cat' is the category ID
+        nom = request.POST.get('nom')
+        description = request.POST.get('description')
+        prix_unitaire = request.POST.get('prix_unitaire')
+        stock = request.POST.get('stock')
+        id_produit = request.POST.get('id_produit')
+        image = request.FILES.get('image')
+        
+        
+        # Retrieve the category instance
+        try:
+            categorie = Categorie.objects.get(nom=categorie_nom)
+        except:
+            categorie = Categorie.objects.create(nom=nom)
+            categorie.save()
+        # Create and save the Produit instance
+        produit = Produit.objects.get(id=id_produit)
+        produit.categorie = categorie
+        produit.nom = nom
+        produit.description = description
+        produit.prix_unitaire = prix_unitaire
+        produit.stock = stock
+        produit.image = image
+        produit.save()
+        # Redirect to a success page or product detail page
+        return redirect(f'/details_produit?id={produit.id}')  # Assuming there's a URL named 'product_detail'
     id  = request.GET.get('id', None)
     produit = Produit.objects.get(id=id)
-    return render(request, 'home/modif_produit.html', {'produit':produit})
+    return render(request, 'home/modif_produit.html', {'produit':produit, 'categories':categories})
 
 def index(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     if request.GET.get('idcat', None) == None:
         produits = Produit.objects.all()
     else:
@@ -92,36 +140,168 @@ def index(request):
 
 
 def ajouter_produit(request):
+    if not request.user.is_authenticated:
+        return redirect('home_login')
+    categories = Categorie.objects.all()
     if request.method == 'POST':
-        pass
-   
-    return render(request, 'home/ajouter_produit.html', {'segment':'ajout'})
+        # Extract data from the form
+        categorie_nom = request.POST.get('cat')  # Assuming the value of 'cat' is the category ID
+        nom = request.POST.get('nom')
+        description = request.POST.get('description')
+        prix_unitaire = request.POST.get('prix_unitaire')
+        stock = request.POST.get('stock')
+        image = request.FILES.get('image')
+        
+        # Retrieve the category instance
+        try:
+            categorie = Categorie.objects.get(nom=categorie_nom)
+        except:
+            categorie = Categorie.objects.create(nom=nom)
+            categorie.save()
+        # Create and save the Produit instance
+        produit = Produit.objects.create(
+            categorie=categorie,
+            nom=nom,
+            description=description,
+            prix_unitaire=prix_unitaire,
+            stock=stock,
+            image=image
+        )
+        produit.save()
+        # Redirect to a success page or product detail page
+        return redirect(f'/details_produit?id={produit.id}')  # Assuming there's a URL named 'product_detail'
+
+    return render(request, 'home/ajouter_produit.html', {'segment': 'ajout', 'categories': categories})
+
+def suppr_produit(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    id  = request.GET.get('id', None)
+    produit = Produit.objects.get(id=id)
+    produit.delete()
+    return redirect('index')
+
+
+def suppr_fact(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    id  = request.GET.get('id', None)
+    fact = Facture.objects.get(id=id)
+    fact.delete()
+    return redirect('fact')
+
+def suppr_cat(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    id  = request.GET.get('id', None)
+    cat = Categorie.objects.get(id=id)
+    cat.delete()
+    return redirect('cat')
+
 
 def home_register(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     return render(request, 'home/register.html')
 
 
 def home_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                return render(request, 'home/login.html', {'error':"nom d'utilisateur ou mot de passe erroné", 'username':username, 'password':password})
+        else:
+            return render(request, 'home/login.html', {'error':"Veillez replir tout les champs"})
+            
     return render(request, 'home/login.html')
 
 
-def logout(request):
-    return render(request, 'home/index.html')
+def logout_view(request):
+    logout(request)
+    return redirect('home_login')
 
 
 def icons(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     return render(request, 'home/icons.html')
 
+
 def fact(request):
-    return render(request, 'home/fact.html', {'segment':'fact'})
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    factures = Facture.objects.all()
+    return render(request, 'home/fact.html', {'segment':'fact', 'factures':factures})
+
+def details_fact(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    id_fact = request.GET.get('id_fact')
+    facture = Facture.objects.get(id=int(id_fact))
+    lignesfactures = LigneFacture.objects.filter(facture=facture)
+    return render(request, 'home/details_fact.html', {'segment':'fact', 'facture':facture, 'lignesfactures':lignesfactures})
 
 def cat(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
     categories = Categorie.objects.all()
     return render(request, 'home/cat.html', {'segment':'cat', 'categories':categories})
 
 
 def user(request):
-    return render(request, 'home/user.html', {'segment':'user'})
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    accounts = User.objects.all()
+    return render(request, 'home/user.html', {'segment':'user', 'accounts':accounts})
+
+
+def createuser(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        if not  nom :
+            error = 'Vous devez entrer un nom'
+        elif pass1 and pass2:
+            if pass1!= pass2:
+                error = 'Les mots de passe ne correspondent pas'
+            else:
+                user = User.objects.create_user(username=nom, password=pass1)
+                user.save()
+                return redirect('user')
+    return render(request, 'home/createuser.html')
+
+def modif_user(request):
+    if not request.user.is_authenticated:
+         return redirect('home_login')
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        nom = request.POST.get('nom')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        if not  nom :
+            error = 'Vous devez entrer un nom'
+        elif pass1 and pass2:
+            if pass1!= pass2:
+                error = 'Les mots de passe ne correspondent pas'
+            else:
+                user = User.objects.get(id=id)
+                user.username = nom
+                user.set_password(pass1)
+                user.save()
+                return redirect('user')
+    id = request.GET.get('id')
+    account = User.objects.get(id=id)
+    return render(request, 'home/modif_user.html', {'segment':'user', 'account':account})
+
 
 
 
