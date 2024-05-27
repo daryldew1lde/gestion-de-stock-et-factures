d3c09c6c97149f831f0da0f327ed98b91204d52f
Daryl Dewilde <daryldewilde@gmail.com>
2024-05-27 09:41:59 +0100

    made changes

--- stocks/App/static/assets/js/myscript.js
+++ stocks/App/static/assets/js/myscript.js
@@ -2,7 +2,14 @@ $('#printInvoice').click(function(){
     Popup($('.invoice')[0].outerHTML);
     function Popup(data) 
     {
+        var fact = document.querySelector("#invoice").innerHTML;
+        var body = document.body.innerHTML;
+        document.body.innerHTML = fact;
+        console.log(document.body.innerHTML);
         window.print();
+        document.body.innerHTML = body;
         return true;
     }
 });
+
+
